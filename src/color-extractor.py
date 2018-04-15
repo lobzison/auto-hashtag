678d11b4238fa68458449ebc8a18b6882e6295a0
Gleb <lobzison@gmail.com>
2018-04-15 23:28:43 +0200

    added color list

--- src/color-extractor.py
+++ src/color-extractor.py
@@ -4,6 +4,17 @@ from PIL import Image
 colors = colorgram.extract(r'D:\python\auto-hashtag\src\input7.png', 6)
 
 for color in colors:
+    print(color.hsl)
+    print(color.proportion)
+    print("----")
+
+print("----_____------")
+
+colors.sort(key=lambda x: x.hsl.s, reverse=True)
+
+
+for color in colors:
+    print(color.hsl)
     print(color.rgb)
     print(color.proportion)
     print("----")
