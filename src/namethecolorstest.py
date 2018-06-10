68c60147746fc0478e1b93f1d4e5d7a9f15563ed
Gleb <lobzison@gmail.com>
2018-06-10 19:05:16 +0200

    added tests

--- src/namethecolorstest.py
+++ src/namethecolorstest.py
@@ -9,14 +9,42 @@ class TestColorExtractor(unittest.TestCase):
     """
     Tests for color extractor module
     """
+    test_colors_1 = NameTheColors("4.jpg")
 
     def test_file_not_exists(self):
         self.assertTrue(True)
         with self.assertRaises(NameError):
             NameTheColors('10.jpg')
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_colors(self):
+        self.assertEquals(set(['Brown', 'Golden Tainoi', 'Neon Carrot',
+                               'Gold', 'Dark Orange', 'Orange', 'Yellow', 'Chocolate']),
+                          self.test_colors_1.get_color_names())
 
-new = NameTheColors('4.jpg')
-print(new.get_color_names())
+    def test_hashtags_initcap_hash(self):
+        self.assertEquals(
+            "#Brown, #Golden Tainoi, #Neon Carrot, #Gold, #Dark Orange, #Orange, #Yellow, #Chocolate, ",
+            self.test_colors_1._create_hashtags())
+
+    def test_hashtags_initcap_nohash(self):
+        self.assertEquals(
+            "Brown, Golden Tainoi, Neon Carrot, Gold, Dark Orange, Orange, Yellow, Chocolate, ",
+            self.test_colors_1._create_hashtags(False))
+
+    def test_hashtags_lower_hash(self):
+        self.assertEquals(
+            "#brown, #golden tainoi, #neon carrot, #gold, #dark orange, #orange, #yellow, #chocolate, ",
+            self.test_colors_1._create_hashtags(True, 'L'))
+
+    def test_hashtags_upper_hash(self):
+        self.assertEquals(
+            "#BROWN, #GOLDEN TAINOI, #NEON CARROT, #GOLD, #DARK ORANGE, #ORANGE, #YELLOW, #CHOCOLATE, ",
+            self.test_colors_1._create_hashtags(True, 'U'))
+
+if __name__ == '__main__':
+    unittest.main()
+
+# new = NameTheColors('4.jpg')
+# print(new._create_hashtags(True, 'U'))
+
+#brown, #golden tainoi, #neon carrot, #gold, #dark orange, #orange, #yellow, #chocolate,
